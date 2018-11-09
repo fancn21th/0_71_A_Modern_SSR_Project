642a20013081b1babe50b592da0bc22cab3579b6
alex.fan <alex.fan@okchem.com>
2018-11-09 13:14:38 +0800

    header components

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,16 @@ export default class MyDocument extends Document {
     return (
       <html lang="en">
         <Head />
-        <body>
+        <body
+          style={{
+            font: '16px Muli',
+            color: '#222',
+            margin: '0px auto',
+            fontWeight: '300',
+            lineHeight: '1.5em',
+            backgroundColor: '#F7F9FC',
+          }}
+        >
           <Main />
           <NextScript />
         </body>
